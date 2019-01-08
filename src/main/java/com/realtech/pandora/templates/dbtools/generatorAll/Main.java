b30f799aa49c23cdf968d4480977b989b5d3e5cc
hyb <hyb@1700.cn>
2019-01-08 11:11:52 +0800

    ps 改造完成

--- src/main/java/com/realtech/pandora/templates/dbtools/generatorAll/Main.java
+++ src/main/java/com/realtech/pandora/templates/dbtools/generatorAll/Main.java
@@ -2,15 +2,17 @@ package com.realtech.pandora.templates.dbtools.generatorAll;
 
 import com.realtech.pandora.templates.dbtools.Config;
 import com.realtech.pandora.templates.dbtools.exception.GeneratorException;
+import com.realtech.pandora.templates.dbtools.generatorP.PsInsertListCopy;
 import com.realtech.pandora.templates.dbtools.model.Table;
 import com.realtech.pandora.templates.dbtools.model.db.DBBase;
 import com.realtech.pandora.templates.dbtools.model.db.DBManager;
 import com.realtech.pandora.util.FMGenerator;
 import com.realtech.pandora.util.StringUtil;
 import freemarker.template.TemplateException;
+import org.junit.Test;
 //import org.python.antlr.ast.Str;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,13 +26,16 @@ public class Main {
         List<String> tableL = db.queryAllTableNames();
         int index = 1, generated = 0;
         List<String> errGenerateTableNames = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
         for(String tableName: tableL) {
 //            System.out.print("全表生成：[ " + index++ + "/" + tableL.size() + " ][" + tableName + "]");
             Table table;
+
             try {
                 table = db.queryTables(tableName);
                 String s = StringUtil.toUpperCamelCase(tableName);
-                System.out.println(  s+ ","+table.getComment()+","+"PSProcessorQLL");
+                System.out.println(  "\""+s+ "\",\""+table.getComment()+"\",\""+"PSProcessorQLL\",");
+                sb.append("\""+s+ "\",\""+table.getComment()+"\",\""+"PSProcessorQLL\",").append("\n");
                 new FMGenerator(Config.properties, table)
                         .replace("$className", table.getClassName())
                         .replace("$package", Config.basePackage.replace(".", "/"))
@@ -48,10 +53,61 @@ public class Main {
                 System.out.println("error[ " + e.getMessage() + " ]");
             }
         }
+        try {
+            byteOutStream(sb.toString(),"ps");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println("生成器执行结束");
         System.out.println("-->生成： " + generated + "/" + tableL.size());
         if(generated < tableL.size()) {
             System.out.println("-->错误： " + errGenerateTableNames);
         }
     }
+
+    public static void byteOutStream(String msg,String fileNmae) throws Exception {
+        //1:使用File类创建一个要操作的文件路径
+        File file = new File("D:" + File.separator + "ps" + File.separator + fileNmae+".txt");
+        if(!file.getParentFile().exists()){ //如果文件的目录不存在
+            file.getParentFile().mkdirs(); //创建目录
+        }
+        //2: 实例化OutputString 对象
+        OutputStream output = new FileOutputStream(file);
+        //3: 准备好实现内容的输出
+        //将字符串变为字节数组
+        byte data[] = msg.getBytes();
+        output.write(data);
+        //4: 资源操作的最后必须关闭
+        output.close();
+
+    }
+
+    @Test
+    public  void getStr() throws Exception {
+        String encoding = "UTF-8";
+        File file = new File("D:\\ps\\ps.txt");
+        if (file.isFile() && file.exists()) { //判断文件是否存在
+            InputStreamReader read = new InputStreamReader(
+                    new FileInputStream(file), encoding);//考虑到编码格式
+            BufferedReader bufferedReader = new BufferedReader(read);
+            String lineTxt = null;
+            PsInsertListCopy psInsertListCopy = new PsInsertListCopy();
+            while ((lineTxt = bufferedReader.readLine()) != null) {
+//                System.out.println(lineTxt);
+                psInsertListCopy.fieldToAdds.append(lineTxt);
+            }
+//            System.out.println(psInsertListCopy.fieldToAdds);
+            String[] split = psInsertListCopy.fieldToAdds.toString().split(",");
+            for(int j = 0; j < split.length; j += 1){
+                psInsertListCopy.fieldToAdd.add(split[j]);
+            }
+            psInsertListCopy.getEnd();
+
+            read.close();
+        } else {
+            System.out.println("找不到指定的文件");
+        }
+    }
+
+
 }
---
+++ src/main/java/com/realtech/pandora/templates/dbtools/generatorP/PsInsertListCopy.java
@@ -0,0 +1,285 @@
+package com.realtech.pandora.templates.dbtools.generatorP;
+
+import com.realtech.pandora.templates.dbtools.generatorAll.*;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by dell on 2017/6/22.
+ */
+public class PsInsertListCopy {
+
+
+    //TODO 每次修改下面这个数组，执行main方法直接生成 ps 服务所有相关内容
+    /**
+     * beanName1 数据库表对应的 bean 的类名
+     * tableName1 数据库表对应的 中文翻译
+     */
+    public final List<String> fieldToAdd = new ArrayList<>();
+    public  final StringBuilder fieldToAdds = new StringBuilder();
+
+
+    // 以下不用填写
+    public static String _TableNameL = "";
+    public static String _TableNameH = "";
+    public static String _TableNameV = "";
+    public static final char UNDERLINE = '_';
+
+    public String camelToUnderline(String param) {
+        if (param == null || "".equals(param.trim())) {
+            return "";
+        }
+        int len = param.length();
+        StringBuilder sb = new StringBuilder(len);
+        for (int i = 0; i < len; i++) {
+            char c = param.charAt(i);
+            if (Character.isUpperCase(c)) {
+                if (i != 0) {
+                    sb.append(UNDERLINE);
+                }
+                sb.append(Character.toLowerCase(c));
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    public void generaPSServerSocketHandler(String tableName, String chinaseTableName, String psName) {
+
+        _TableNameL = camelToUnderline(tableName);
+        _TableNameH = _TableNameL.toUpperCase();
+        _TableNameV = tableName.substring(0, 1).toLowerCase() + tableName.substring(1);
+        //System.out.println(_TableNameL + "****" + _TableNameH+ "*****" + _TableNameV);
+        System.out.println("/* " + tableName + "<" + chinaseTableName + "> */\n" +
+                "\t\t\t\telse if(pojo.getClazz().equals(" + tableName + ".class.getName())) {\n" +
+                "\t\t\t\t\t" + psName + " Ps = new " + psName + "();\n" +
+                "    \t\t\t\ttry {\n" +
+                "    \t\t\t\t\tSqlReplyModel curSqlReplyModel = " + psName + ".process(BillTypeEnum." + _TableNameH + ", pojo, sqlSession, referProcessor);\n" +
+                "    \t\t\t\t\tif(curSqlReplyModel == null) {\n" +
+                "    \t\t\t\t\t\t/* 加载'writer'失败 */\n" +
+                "    \t\t\t\t\t\tlogger.error(\"load Writer failed!\");\n" +
+                "\n" +
+                "    \t\t\t\t\t\t/* 设置‘失败’标志 */\n" +
+                "    \t\t\t\t\t\tsuccess = false;\n" +
+                "\n" +
+                "    \t\t\t\t\t\t/* 设置‘失败点’现场信息 */\n" +
+                "    \t\t\t\t\t    sqlExecResult.append(\"load Writer failed!\");\n" +
+                "\t\t\t\t\t\t    break;\n" +
+                "    \t\t\t\t\t}\n" +
+                "\n" +
+                "    \t\t\t\t\tif(curSqlReplyModel.getSqlStatus() != SqlStatusEnum.SQL_EXEC_SUCCEED) {\n" +
+                "    \t\t\t\t\t\t/* 原子SQL语句执行失败 */\n" +
+                "    \t\t\t\t\t\tlogger.error(\"process \\'" + tableName + "\\' model failed!\");\n" +
+                "\n" +
+                "    \t\t\t\t\t\t/* 设置‘失败’标志 */\n" +
+                "    \t\t\t\t\t\tsuccess = false;\n" +
+                "    \t\t\t\t\t\t\n" +
+                "    \t\t\t\t\t\t/* 设置‘失败点’现场信息 */\n" +
+                "    \t\t\t\t\t    sqlExecResult.append(curSqlReplyModel.getSqlClause());\n" +
+                "    \t\t\t\t\t    sqlExecResult.append(\"|\");\n" +
+                "    \t\t\t\t\t    sqlExecResult.append(curSqlReplyModel.getSqlStatus());\n" +
+                "    \t\t\t\t\t    sqlExecResult.append(\";\");\n" +
+                "\t\t\t\t\t\t    break;\n" +
+                "    \t\t\t\t\t}\n" +
+                "    \t\t\t\t}\n" +
+                "    \t\t\t\tcatch(UnknownModelTypeException umt) {\n" +
+                "    \t\t\t\t\t/* '" + psName + "'不支持'" + tableName + "'的处理 */\n" +
+                "    \t\t\t\t\tlogger.error(\"\\'" + tableName + "\\' is not supported by \\'" + psName + "\\' currently!\");\n" +
+                "\n" +
+                "\t\t\t\t\t\t/* 设置‘失败’标志 */\n" +
+                "\t\t\t\t\t\tsuccess = false;\n" +
+                "\n" +
+                "\t\t\t\t\t\t/* 设置‘失败点’现场信息 */\n" +
+                "\t\t\t\t\t    sqlExecResult.append(\"\\'" + tableName + "\\' is not supported by \\'" + psName + "\\' currently!\");\n" +
+                "\t\t\t\t\t    break;\n" +
+                "    \t\t\t\t}\n" +
+                "\t\t\t\t}");
+        System.out.println();
+
+    }
+
+    public void generaPSProcessor(String tableName, String chinaseTableName,StringBuilder sbGeneraPSProcessor) {
+        _TableNameL = camelToUnderline(tableName);
+        _TableNameH = _TableNameL.toUpperCase();
+        _TableNameV = tableName.substring(0, 1).toLowerCase() + tableName.substring(1);
+/*        System.out.println("else if(pojo_type == BillTypeEnum." + _TableNameH + ") {\n" +
+                "\t\t\t*//* 执行'" + chinaseTableName + "'实体数据库操作 *//*\n" +
+                "\t\t\t" + tableName + " " + _TableNameV + " = (" + tableName + ")pojo;\n" +
+                "\t\t\treturn process" + tableName + "(" + _TableNameV + ", sql_session, refer_processor);\n" +
+                "\t\t}");*/
+        sbGeneraPSProcessor.append("else if(pojo_type == BillTypeEnum." + _TableNameH + ") {\n" +
+                "\t\t\t/* 执行'" + chinaseTableName + "'实体数据库操作 */\n" +
+                "\t\t\t" + tableName + " " + _TableNameV + " = (" + tableName + ")pojo;\n" +
+                "\t\t\treturn process" + tableName + "(" + _TableNameV + ", sql_session, refer_processor);\n" +
+                "\t\t}").append("\n");
+    }
+
+    public void generaPSProcessor2(String tableName, String chinaseTableName,StringBuilder sbGeneraPSProcessor) {
+        _TableNameL = camelToUnderline(tableName);
+        _TableNameH = _TableNameL.toUpperCase();
+        _TableNameV = tableName.substring(0, 1).toLowerCase() + tableName.substring(1);
+        /*System.out.println("*//**\n" +
+                "\t * 执行'" + tableName + "<" + chinaseTableName + ">'的数据库持久化原子操作\n" +
+                "\t * @param " + _TableNameL + "\n" +
+                "\t * @param sql_session\n" +
+                "\t * @param refer_processor\n" +
+                "\t * @return SqlReplyModel\n" +
+                "\t * @throws\n" +
+                "\t *//*\n" +
+                "\tpublic SqlReplyModel process" + tableName + "(" + tableName + " " + _TableNameL + ", SqlSession sql_session, ReferenceProcessor<BaseModel> refer_processor) {\n" +
+                "\t\tPersistenceUtil<" + tableName + "> persister = new PersistenceUtil<" + tableName + ">();\n" +
+                "\t\treturn persister.persist(" + _TableNameL + ", sql_session, " + tableName + "Writer.class, refer_processor);\n" +
+                "\t}");
+        System.out.println();*/
+        sbGeneraPSProcessor.append("/**\n" +
+                "\t * 执行'" + tableName + "<" + chinaseTableName + ">'的数据库持久化原子操作\n" +
+                "\t * @param " + _TableNameL + "\n" +
+                "\t * @param sql_session\n" +
+                "\t * @param refer_processor\n" +
+                "\t * @return SqlReplyModel\n" +
+                "\t * @throws\n" +
+                "\t */\n" +
+                "\tpublic SqlReplyModel process" + tableName + "(" + tableName + " " + _TableNameL + ", SqlSession sql_session, ReferenceProcessor<BaseModel> refer_processor) {\n" +
+                "\t\tPersistenceUtil<" + tableName + "> persister = new PersistenceUtil<" + tableName + ">();\n" +
+                "\t\treturn persister.persist(" + _TableNameL + ", sql_session, " + tableName + "Writer.class, refer_processor);\n" +
+                "\t}").append("\n");
+    }
+
+    public void generaBillTypeEnum(String tableName, String chinaseTableName,StringBuilder sbGeneraBillTypeEnum) {
+        _TableNameL = camelToUnderline(tableName);
+        _TableNameH = _TableNameL.toUpperCase();
+        _TableNameV = tableName.substring(0, 1).toLowerCase() + tableName.substring(1);
+//        System.out.println(_TableNameH + "(" + "code" + ", \"" + tableName + "\", \"" + _TableNameL + "\", \"" + chinaseTableName + "\", \"" + _TableNameL + "\"),");
+        sbGeneraBillTypeEnum.append(_TableNameH + "(" + "code" + ", \"" + tableName + "\", \"" + _TableNameL + "\", \"" + chinaseTableName + "\", \"" + _TableNameL + "\"),");
+    }
+
+    public void getEnd(){
+        StringBuilder sbGeneraPSServerSocketHandler1 = new StringBuilder();
+        StringBuilder sbGeneraPSProcessor = new StringBuilder();
+        StringBuilder sbGeneraPSProcessor2 = new StringBuilder();
+        StringBuilder sbGeneraBillTypeEnum = new StringBuilder();
+
+        PsInsertListCopy ps = new PsInsertListCopy();
+        System.out.println("________________________________ PSServerSocketHandler.java___________________________________________________________________");
+        System.out.println("_____________________________________________________________________________________________________________________________");
+        System.out.println();
+        for (int j = 0; j < fieldToAdd.size(); j += 3) {
+            ps.generaPSServerSocketHandler1(fieldToAdd.get(j), fieldToAdd.get(j+1), fieldToAdd.get(j+2),sbGeneraPSServerSocketHandler1);
+        }
+        try {
+            com.realtech.pandora.templates.dbtools.generatorAll.Main.byteOutStream(sbGeneraPSServerSocketHandler1.toString(),"PSServerSocketHandler");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        System.out.println("________________________________ PSProcessor.java____________________________________________________________________________");
+        System.out.println("_____________________________________________________________________________________________________________________________");
+        for (int j = 0; j < fieldToAdd.size(); j += 3) {
+            ps.generaPSProcessor(fieldToAdd.get(j), fieldToAdd.get(j+1),sbGeneraPSProcessor);
+        }
+        try {
+            com.realtech.pandora.templates.dbtools.generatorAll.Main.byteOutStream(sbGeneraPSProcessor.toString(),"PSProcessor1");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println();
+        System.out.println("===============================================================================");
+        for (int j = 0; j < fieldToAdd.size(); j += 3) {
+            ps.generaPSProcessor2(fieldToAdd.get(j), fieldToAdd.get(j+1),sbGeneraPSProcessor2);
+        }
+        try {
+            com.realtech.pandora.templates.dbtools.generatorAll.Main.byteOutStream(sbGeneraPSProcessor2.toString(),"PSProcessor2");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("________________________________ BillTypeEnum.java __________________________________________________________________________");
+        System.out.println("_____________________________________________________________________________________________________________________________");
+        for (int j = 0; j < fieldToAdd.size(); j += 3) {
+            ps.generaBillTypeEnum(fieldToAdd.get(j), fieldToAdd.get(j+1),sbGeneraBillTypeEnum);
+        }
+        try {
+            com.realtech.pandora.templates.dbtools.generatorAll.Main.byteOutStream(sbGeneraBillTypeEnum.toString(),"BillTypeEnum");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void generaPSServerSocketHandler1(String tableName, String chinaseTableName, String psName,StringBuilder sbGeneraPSServerSocketHandler1) {
+        _TableNameL = camelToUnderline(tableName);
+        _TableNameH = _TableNameL.toUpperCase();
+        _TableNameV = tableName.substring(0, 1).toLowerCase() + tableName.substring(1);
+/*        System.out.println("*//* " + tableName + "<" + chinaseTableName + "> *//*\n" +
+                "\t\t\t\telse if(pojo.getClazz().equals(" + tableName + ".class.getName())) {\n" +
+                "\t\t\t\tsuccess = new ModelHandler(sqlExecResult, BillTypeEnum." + _TableNameH + ", new "+psName+"()).execute(pojo, sqlSession, referProcessor);\n" +
+                "\t\t\t\t}");
+        System.out.println();*/
+        sbGeneraPSServerSocketHandler1.append("/* " + tableName + "<" + chinaseTableName + "> */\n" +
+                "\t\t\t\telse if(pojo.getClazz().equals(" + tableName + ".class.getName())) {\n" +
+                "\t\t\t\tsuccess = new ModelHandler(sqlExecResult, BillTypeEnum." + _TableNameH + ", new "+psName+"()).execute(pojo, sqlSession, referProcessor);\n" +
+                "\t\t\t\t}").append("\n");
+
+    }
+
+    final static String cfn = "net.sourceforge.jtds.jdbc.Driver";
+    final static String url = "jdbc:jtds:sqlserver://192.168.0.8:1433/eohi-yun-db";
+
+    @Test
+    public void fieldToAdd (){
+        StringBuilder fieldToAdd = new StringBuilder();
+        Connection con = null;
+        PreparedStatement statement = null;
+        ResultSet res = null;
+        try {
+            Class.forName(cfn);
+            con = DriverManager.getConnection(url,"sa","Admin2012");
+
+            String sql = "SELECT\n" +
+                    "\tc.name,\n" +
+                    "\tCAST (\n" +
+                    "\t\tisnull(f.[value], '') AS nvarchar (100)\n" +
+                    "\t) AS remark\n" +
+                    "FROM\n" +
+                    "\tsys.objects c\n" +
+                    "LEFT JOIN sys.extended_properties f ON f.major_id = c.object_id\n" +
+                    "AND f.minor_id = 0\n" +
+                    "AND f.class = 1\n" +
+                    "WHERE\n" +
+                    "\tc.type = 'u'";//查询test表
+            statement = con.prepareStatement(sql);
+            res = statement.executeQuery();
+            while(res.next()){
+                String title = res.getString("name");//获取test_name列的元素                                                                                                                                                    ;
+                System.out.println("姓名："+title);
+                String remark = res.getString("remark");//获取test_name列的元素                                                                                                                                                    ;
+                System.out.println("姓名："+remark);
+                fieldToAdd.append(title).append(",").append(remark).append(",");
+            }
+            System.out.println(fieldToAdd);
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }finally{
+            try {
+                if(res != null) res.close();
+                if(statement != null) statement.close();
+                if(con != null) con.close();
+            } catch (Exception e2) {
+                // TODO: handle exception
+                e2.printStackTrace();
+            }
+        }
+    }
+
+
+}
